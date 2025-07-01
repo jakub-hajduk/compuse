6436fe1deba691f13d0ac92759be1a761aa993ff
Jakub Hajduk <devhajduk@gmail.com>
2025-07-01 08:43:38 +0200

    chore: add index

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './engine/analyze-code';
+export * from './engine/analyze-file';
+
+export * from './analyzers/vue/vue.analyzer';
+export * from './analyzers/angular/angular.analyzer';
+export * from './analyzers/react/react.analyzer';
